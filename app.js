aa89f001ac07d04c56b5c9f4eb4ba230500d7651
Blake Enloe <blakewenloe@yahoo.com>
2020-11-04 18:21:03 -0600

    added getMode function

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ const getMedian = (arr) => {
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
 
+const getMode = (nums) => {
+  var modes = [],
+    count = [],
+    i,
+    nums,
+    maxIndex = 0;
+
+  for (i = 0; i < nums.length; i += 1) {
+    number = nums[i];
+    count[number] = (count[number] || 0) + 1;
+    if (count[number] > maxIndex) {
+      maxIndex = count[number];
+    }
+  }
+
+  for (i in count)
+    if (count.hasOwnProperty(i)) {
+      if (count[i] === maxIndex) {
+        modes.push(Number(i));
+      }
+    }
+
+  return modes;
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,7 +52,10 @@ app.get("/median/:nums", (req, res) => {
   res.json({ operation: "median", value: getMedian(nums) });
 });
 
-app.get("/mode/:nums", (req, res) => {});
+app.get("/mode/:nums", (req, res) => {
+  let nums = req.params.nums.split(",");
+  res.json({ operation: "median", value: getMode(nums) });
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
